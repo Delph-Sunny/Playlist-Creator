7841c81885a1df6f49d3f18b5f3c0b8d51df5cd8
Delph <delph_teb@hotmail.com>
2020-12-12 17:17:40 -0500

    minor changes

--- assets/main.js
+++ assets/main.js
@@ -9,14 +9,14 @@ $(document).ready(function () {
             playlistName: "My running",
             description: "for Saturday workout",
             icon: "play-arrow",
-            iconColor: "red"
+            iconColor: "red",
         },
         {
             index: 1,
             playlistName: "My second playlist",
             description: "for Sunday workout",
             icon: "android",
-            iconColor: "white"
+            iconColor: "white",
         }
         ,
         {
@@ -24,7 +24,7 @@ $(document).ready(function () {
             playlistName: "My third playlist",
             description: "for week workout",
             icon: "beach_access",
-            iconColor: "blue"
+            iconColor: "blue",
         }
     ]
     localStorage.setItem("playlistsList", JSON.stringify(userList));
@@ -39,12 +39,13 @@ $(document).ready(function () {
         for (let i = 0; i < userList.length; i++) {
             var playlistEl = $("<li>");
             // Adding a class, attribute and text
-            playlistEl.addClass("collection-item avatar playlist-name");
+            playlistEl.addClass("collection-item avatar");
             playlistEl.attr("data-name", userList[i].playlistName);
             playlistEl.attr("data-index", i);
-            playlistEl.text(userList[i].playlistName);
-            playlistEl.append(`<a class="btn-floating btn-small waves-effect waves-light indigo" id="clear" data-index="${i}">
-        <i class="material-icons">clear</i></a>`)
+            playlistEl.append(` <i class="material-icons circle ${userList[i].iconColor}">${userList[i].icon}</i>
+            <span class="title playlist-title">${userList[i].playlistName}</span>
+            <p>${userList[i].description}</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">clear</i></a>`)
             $("#playlists").append(playlistEl);
         };
     };
@@ -52,7 +53,7 @@ $(document).ready(function () {
     playlistBuild();
 
     // when clicking on a playlist button
-    $(document).on("click", ".playlist-name", function (event) {
+    $(document).on("click", ".collection-item", function (event) {
         event.preventDefault();
         userChoice = ($(this).data('index'));
 
@@ -62,7 +63,7 @@ $(document).ready(function () {
 
     // for Clear All button
     $("#clear-all").on("click", function (event) {
-        M.toast({ html: `Are you sure you want to delete all playlists?`, classes: 'rounded' })
+        //   M.toast({ html: `Are you sure you want to delete all playlists?`, classes: 'rounded' })
         userList = [];
         localStorage.clear();
         /* Disable btn once used */
@@ -72,19 +73,19 @@ $(document).ready(function () {
 
     /****BROKEN*****/
     // for remove 1 playlist at the time
-    var resetIndex = function () {      // reinitilize index value in object
-        $('div').each(function (i) {
-            $(this).attr("data-index", i)
-            userList[i].index = i;
-        });
-        localStorage.setItem("playlistsList", JSON.stringify(userList));
-    }
+    //  var resetIndex = function () {      // reinitilize index value in object
+    //       $('div').each(function (i) {
+    //           $(this).attr("data-index", i)
+    //           userList[i].index = i;
+    //       });
+    //       localStorage.setItem("playlistsList", JSON.stringify(userList));
+    //   }
 
-    $(document).on('click', '#clear', function () {
-        const playlistEl = $("<div>");
-        let i = $(this).data('index');
-        userList.splice(i, 1);
-        $.when($(`.playlist-name:eq(${i})`).remove()).then(resetIndex());
-    })
+    //   $(document).on('click', '#remove', function () {
+    //       const playlistEl = $("<div>");
+    //      let i = $(this).data('index');
+    //      userList.splice(i, 1);
+    //       $.when($(`.playlist-name:eq(${i})`).remove()).then(resetIndex());
+    //   })
 
 })
